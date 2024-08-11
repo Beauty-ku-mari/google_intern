9d2f82389a2a38bf5ca195a04793e99768ca7ca1
Beauty kumari <kbeauty193@gmail.com>
2024-08-11 17:35:45 +0530

    'add'

--- Training_Data_Check.py
+++ Training_Data_Check.py
@@ -1,66 +1,24 @@
-import cv2
-import numpy as np
-from scipy import misc
-import matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images=training_images.reshape(60000, 28, 28, 1)
+training_images=training_images / 255.0
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images=test_images / 255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print ('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy*100))
 
 
-i = misc.ascent()
-
-
-i_transformed = np.copy(i)
-size_x = i_transformed.shape[0]
-size_y = i_transformed.shape[1]
-
-#******************original img show
-
-# plt.grid(False)
-# plt.gray()
-# plt.axis('off')
-# plt.imshow(i)
-# plt.show()
-
-
-
-# This filter detects edges nicely
-# It creates a filter that only passes through sharp edges and straight lines. 
-# Experiment with different values for fun effects.
-#filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]] 
-# A couple more filters to try for fun!
-filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
-#filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
- # If all the digits in the filter don't add up to 0 or 1, you 
-# should probably do a weight to get it to do so
-# so, for example, if your weights are 1,1,1 1,2,1 1,1,1
-# They add up to 10, so you would set a weight of .1 if you want to normalize them
-weight  = 1
-
-
-
-for x in range(1,size_x-1):
-  for y in range(1,size_y-1):
-      output_pixel = 0.0
-      output_pixel = output_pixel + (i[x - 1, y-1] * filter[0][0])
-      output_pixel = output_pixel + (i[x, y-1] * filter[0][1])
-      output_pixel = output_pixel + (i[x + 1, y-1] * filter[0][2])
-      output_pixel = output_pixel + (i[x-1, y] * filter[1][0])
-      output_pixel = output_pixel + (i[x, y] * filter[1][1])
-      output_pixel = output_pixel + (i[x+1, y] * filter[1][2])
-      output_pixel = output_pixel + (i[x-1, y+1] * filter[2][0])
-      output_pixel = output_pixel + (i[x, y+1] * filter[2][1])
-      output_pixel = output_pixel + (i[x+1, y+1] * filter[2][2])
-      output_pixel = output_pixel * weight
-      if(output_pixel<0):
-        output_pixel=0
-      if(output_pixel>255):
-        output_pixel=255
-      i_transformed[x, y] = output_pixel
-
-
-      # Plot the image. Note the size of the axes -- they are 512 by 512
-
-#******************filter img show
-plt.gray()
-plt.grid(False)
-plt.imshow(i_transformed)
-plt.axis('off')
-plt.show()   
